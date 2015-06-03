7e7f992622b7e9b8bb3705c1c680ffb355ae9702
Richard Macias <RJNY86@gmail.com>
2015-06-03 14:31:53 -0400

    initialize survey controller

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,10 @@
+class SurveysController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'surveys#index'
+  get 'surveys/new'
+  get 'surveys/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
